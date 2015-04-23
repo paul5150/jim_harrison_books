5bbb5e699f5daa5ee1080ae08748b369645e88aa
Paul Morin <pmorin35@gmail.com>
2015-04-23 06:38:12 -0700

    starting to add some css

--- app.rb
+++ app.rb
@@ -8,11 +8,15 @@ get('/') do
     <title>Jim Harrison</title>
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
   </head>
+  <body>
+  <div class='container'>
     <h1>Jim Harrison</h1>
     <img src='/img/harrison.jpg' alt='photo of Jim Harrison'/>
     <h2>The best American writer you've never read.</h2>
     <h2>Please fix that. Now.</h2>
     <p><a href='/books'>Here are some great books to get you started!</a></p>
+  </div>
+  </body>
   </html>"
 end
 
@@ -23,12 +27,14 @@ get('/books') do
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
   </head>
   <body>
+  <div class='container'>
     <h1>Some books to get you started</h1>
     <ul>
       <li>Dalva</li>
       <li>The Woman Lit by Fireflies</li>
       <li>Returning to Earth: A Novel</li>
     </ul>
+  </div>  
   </body>
   </html>"
 end
